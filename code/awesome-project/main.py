390421664b419db943f94a6f324ee9ad6058d8e4
Loïc Jeanneret <lol.jeanneret@gmail.com>
2024-11-20 15:27:47 +0000

    select

--- code/awesome-project/main.py
+++ code/awesome-project/main.py
@@ -32,9 +32,11 @@ def create_heroes():
 
 
 def select_heroes():
-    with Session(engine) as session:  
-        heroes = session.exec(select(Hero)).all()
-        print(heroes)  
+    with Session(engine) as session:
+        statement = select(Hero).where(Hero.name == "Deadpond")
+        results = session.exec(statement)
+        for hero in results:
+            print(hero)
     
 
 
